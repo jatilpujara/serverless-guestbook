4a3720aa85ceddbbf788c442d80eb96fb4199115
jatilpujara <57207225+jatilpujara@users.noreply.github.com>
2022-04-21 17:20:54 +0530

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://1577289c.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
